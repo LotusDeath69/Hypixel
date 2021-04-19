5f5395357237f448f2e823a80e3951fd129c520b
LotusDeath <LotusDeath69@gmail.com>
2021-04-19 18:48:35 -0400

    First Commit

---
+++ Stats.py
@@ -0,0 +1,94 @@
+import requests
+import datetime
+from secrets import api
+name = 'stepsisters'
+
+
+def uuid(ign):
+    # if ign.lower() == 'thatbananaking':
+    #     ThatBananaKing()
+    try:
+        data = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{ign}').json()
+        return data['id']
+    except ValueError:
+        # print('Decoding JSON has failed')
+        print('Invalid username \nPlease try again')
+        exit()
+
+
+def stats(ign, key):
+    data = requests.get(f'https://api.hypixel.net/player?key={key}&uuid={uuid(ign)}').json()
+    unix_time_first_joined = data['player']['firstLogin']
+    normal_time_first_joined = datetime.datetime.fromtimestamp(unix_time_first_joined/1000.0)
+    unix_time_last_joined = data['player']['lastLogin']
+    normal_time_last_joined = datetime.datetime.fromtimestamp(unix_time_last_joined/1000.0)
+
+    achievement_points =  data['player']['achievementPoints']
+    karma = data['player']['karma']
+    rank = getRank(data)
+    current_level = getLevel(data['player']['networkExp'])
+    current_guild = getGuild(ign, key)
+    friend = getFriend(ign, key)
+    status = getStatus(ign, key)
+    return f'Rank: {rank} Guild: {current_guild} Status: {status} Friends: {friend} Level: {current_level} Achievement Point: {achievement_points} Karma: {karma} '\
+    f'First Join: {normal_time_first_joined} Last Joined: {normal_time_last_joined}'
+
+
+def ThatBananaKing():
+    pass 
+
+
+def getRank(data):
+    try:
+        if data['player']['monthlyPackageRank'] == 'none':
+            rank = data['player']['newPackageRank']
+    
+        else: 
+            rank = data['player']['monthlyPackageRank']
+    except KeyError:
+        return 'non'
+    
+    if rank == 'SUPERSTAR':
+        return 'MVP ++'
+    if rank == 'MVP_PLUS':
+        return 'MVP +'
+    if rank == 'VIP_PLUS':
+        return 'VIP +'
+    return rank
+
+
+def getLevel(exp):
+    next_level = 10_000
+    level = 1
+    for _ in range(1000):
+        if exp < next_level:
+            return level
+        else:
+            exp -= next_level
+            next_level += 2_500
+            level += 1
+
+
+def getGuild(ign, key):
+    try:
+        data = requests.get(f'https://api.hypixel.net/findGuild?key={key}&byUuid={uuid(ign)}').json()
+        guild = requests.get(f'https://api.hypixel.net/guild?key={key}&id={data["guild"]}').json()
+        return guild['guild']['name']
+    except KeyError:
+        return f'{ign} is not currently in a guild.'
+
+
+def getFriend(ign, key):
+    data = requests.get(f'https://api.hypixel.net/friends?key={key}&uuid={uuid(ign)}').json()
+    return len(data['records'])
+
+
+def getStatus(ign, key):
+    data = requests.get(f'https://api.hypixel.net/status?key={key}&uuid={uuid(ign)}').json()
+    if data['session'] is True:
+        return 'online'
+    else:
+        return 'offline'
+
+
+print(stats(name, api))
---
+++ test.py
@@ -0,0 +1,3 @@
+import datetime
+a = datetime.datetime.fromtimestamp(1489174064654/1000.0)
+
